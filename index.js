7b2815cdbdec51f18af5fb575d758360844a3ca0
Gabriel BRUN <lydstyl@gmail.com>
2018-12-20 12:41:20 +0100

    algo ok to code for remove duplicates

--- index.js
+++ index.js
@@ -27,10 +27,39 @@ if (opts.mode == 'extract') {
     
     getToTranslateSync() // fr_FR --> "_fr_FR.properties"
 }
+if (opts.mod == 'remove duplicates') {
+    // const rmDuplicateKeys = require('./main/rmDuplicateKeys')
+    // rmDuplicateKeys()
 
-// const saveChangedKeysInMainCartridge = require('./main/saveChangedKeysInMainCartridge')
-// const rmDuplicateKeys = require('./main/rmDuplicateKeys')
-// const read = require( 'utils-fs-read-properties' )
 
-// saveChangedKeysInMainCartridge()
-// rmDuplicateKeys()
+    // Caroll = A
+
+    // A key:val --> ne pas supprimer
+    // B key:val-différente
+    // C key:val
+
+    // A key:val --> supprimer
+    // B key:val
+    // C key:val1
+
+
+    // si pas de ALL-PROPS.json
+    //     log il faut lancer extract avant !
+
+    // sinon
+    //     let mainProps = {}
+    //     pour chaque mainProp dans main (app_carroll)
+    //         pour chaque mainKey
+    //             let mainVal
+    //             pour chaque cartridge sauf main du plus au moins important
+    //                 pour chaque prop
+    //                     si prop == mainProp
+    //                         pour chaque key
+    //                             si key == mainKey
+    //                                 let val
+    //                                 si val != mainVal
+    //                                     diffKeys.push({propName:propName, key:key}) // ajouter la key dans diffkeys
+    //                                 sinon
+    //                                     si key non présente dans diffKeys
+    //                                         supprimer le couple mainKey et mainVal de mainProps
+}
