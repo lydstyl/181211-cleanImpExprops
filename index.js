241dd4f941cc1645cbe23f8a89a9a0f733e64da3
Gabriel BRUN <lydstyl@gmail.com>
2018-12-20 11:56:39 +0100

    mode import or extract

--- index.js
+++ index.js
@@ -1,29 +1,36 @@
 const opts = require('./opts')
-const extractFrontKeys = require('./main/extractFrontKeys')
-const getEssential = require('./main/getEssential')
-const getToTranslate = require('./main/getToTranslate')
+console.log(`Your opts.json:`)
+console.log(opts)
+
+if (opts.mode == 'import') {
+    const generateTranslatedProps = require('./main/generateTranslatedProps')
+    generateTranslatedProps()
+}
+if (opts.mode == 'extract') {
+    const extractFrontKeys = require('./main/extractFrontKeys')
+    const getEssential = require('./main/getEssential')
+    const getToTranslate = require('./main/getToTranslate')
+    function extractFrontKeysAsync() {
+        return new Promise(resolve => {
+            extractFrontKeys()
+        })
+    }
+    async function extractFrontKeysSync() {
+        await extractFrontKeysAsync()
+    }
+    async function getToTranslateSync() {
+        await getToTranslate(opts.languageToTranslate)
+    }
+    
+    extractFrontKeysSync() // this create ./generated/ALL-PROPS.json
+    getEssential() // this create ./generated/ESSENTIAL.json a file without duplicate
+    
+    getToTranslateSync() // fr_FR --> "_fr_FR.properties"
+}
+
 // const saveChangedKeysInMainCartridge = require('./main/saveChangedKeysInMainCartridge')
 // const rmDuplicateKeys = require('./main/rmDuplicateKeys')
 // const read = require( 'utils-fs-read-properties' )
-
-function extractFrontKeysAsync() {
-    return new Promise(resolve => {
-        extractFrontKeys()
-    })
-}
-async function extractFrontKeysSync() {
-    await extractFrontKeysAsync()
-}
-async function getToTranslateSync() {
-    await getToTranslate(opts.languageToTranslate)
-}
-
-console.log(`Your opts.json:`)
-console.log(opts)
-extractFrontKeysSync() // this create ./generated/ALL-PROPS.json
-getEssential() // this create ./generated/ESSENTIAL.json a file without duplicate
-
-getToTranslateSync() // fr_FR --> "_fr_FR.properties"
 
 // saveChangedKeysInMainCartridge()
 // rmDuplicateKeys()
---
+++ main/generateTranslatedProps.js
@@ -0,0 +1,23 @@
+const fs = require('fs')
+const path = require('path')
+
+module.exports = function () {
+    const opts = require('../opts')
+    const translated = require('../TRANSLATED') // put this path in opts ?
+    Object.keys(translated).forEach(prop => {
+        let content = '#Generate by an Angel\n'
+        let newProp = prop.split('.properties')[0]
+        newProp = newProp + '_' + opts.newLangage + '.properties'
+        let keys = Object.keys(translated[prop])
+        keys = keys.sort()
+        keys.forEach(key => {
+            content += key + ':' + translated[prop][key] + '\n'
+        });
+        fs.writeFileSync(
+            path.join(__dirname, '../generated/translatedProps', newProp),
+            content,
+            'utf8'
+        )
+    });
+}
+// module.exports() // for debug and to comment after
