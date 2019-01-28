675e531c8a82b5cb4d463c81ba7f513527ba1316
Gabriel BRUN <lydstyl@gmail.com>
2019-01-28 10:41:23 +0100

    rmDir ok

---
+++ main/rmDir.js
@@ -0,0 +1,11 @@
+const fs = require('fs')
+const path = require('path')
+const directory = path.join(__dirname, '../generated/props')
+fs.readdir(directory, (err, files) => {
+    if (err) throw err
+    for (const file of files) {
+        fs.unlink(path.join(directory, file), err => {
+            if (err) throw err
+        })
+    }
+})
